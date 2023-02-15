0a9f834a03a23066f52fbb67d81282f680e00096
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2023-02-15 14:22:20 -0700

    Initial Commit

---
+++ ConvertTiles/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ConvertTiles
+//
+//  Created by Jason Koehn on 2/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ConvertTiles/ConvertTilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertTilesApp.swift
+//  ConvertTiles
+//
+//  Created by Jason Koehn on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ConvertTilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
