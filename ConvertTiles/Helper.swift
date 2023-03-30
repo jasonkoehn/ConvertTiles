f8cb101ffb3d3c26352f97c73160d11ffb20c33f
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2023-03-30 09:53:45 -0700

    Updated settings

---
+++ ConvertTiles/Helper.swift
@@ -0,0 +1,8 @@
+//
+//  Helper.swift
+//  ConvertTiles
+//
+//  Created by Jason Koehn on 4/22/23.
+//
+
+import Foundation
---
+++ ConvertTiles/KeychainManager.swift
@@ -0,0 +1,8 @@
+//
+//  KeychainManager.swift
+//  ConvertTiles
+//
+//  Created by Jason Koehn on 4/19/23.
+//
+
+import Foundation
